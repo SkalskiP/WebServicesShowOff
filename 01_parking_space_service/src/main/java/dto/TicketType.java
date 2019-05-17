04c78c238c2a8879ef8ec95eaaebd21c40a563ac
SkalskiP <piotr.skalski92@gmail.com>
2019-05-17 13:44:43 +0200

    parking space database diagram

---
+++ 01_parking_space_service/src/main/java/dto/TicketType.java
@@ -0,0 +1,55 @@
+package dto;
+
+import javax.persistence.*;
+
+@Entity()
+@Table(name = "TICKET_TYPE")
+@Access(AccessType.FIELD)
+public class TicketType {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "duration", nullable = false)
+    private Integer duration;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+}
