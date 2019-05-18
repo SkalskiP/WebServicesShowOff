033e7c57474fda63b37c6ded4c92476dca75cf31
SkalskiP <piotr.skalski92@gmail.com>
2019-05-18 18:55:58 +0200

    database creation script updated

--- 07_parking_space_database/PARKING_SPACE.sql
+++ 07_parking_space_database/PARKING_SPACE.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Czas generowania: 17 Maj 2019, 14:19
+-- Czas generowania: 18 Maj 2019, 18:53
 -- Wersja serwera: 5.7.25-0ubuntu0.18.04.2
 -- Wersja PHP: 7.0.33-5+ubuntu18.04.1+deb.sury.org+1
 
@@ -31,6 +31,15 @@ CREATE TABLE `EMPLOYEE` (
   `id_from_account_db` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Zrzut danych tabeli `EMPLOYEE`
+--
+
+INSERT INTO `EMPLOYEE` (`id`, `id_from_account_db`) VALUES
+(1, 1),
+(2, 2),
+(3, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -39,9 +48,152 @@ CREATE TABLE `EMPLOYEE` (
 
 CREATE TABLE `PARKING_SPOT` (
   `id` int(11) NOT NULL,
-  `number` int(3) NOT NULL,
+  `number` int(11) NOT NULL,
+  `occupied` tinyint(1) NOT NULL DEFAULT '0',
   `street_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `PARKING_SPOT`
+--
+
+INSERT INTO `PARKING_SPOT` (`id`, `number`, `occupied`, `street_id`) VALUES
+(113, 1, 0, 1),
+(114, 2, 0, 1),
+(115, 3, 0, 1),
+(116, 4, 0, 1),
+(117, 5, 0, 1),
+(118, 6, 0, 1),
+(119, 7, 0, 1),
+(120, 1, 0, 2),
+(121, 2, 0, 2),
+(122, 3, 0, 2),
+(123, 4, 0, 2),
+(124, 5, 0, 2),
+(125, 1, 0, 3),
+(126, 2, 0, 3),
+(127, 3, 0, 3),
+(128, 4, 0, 3),
+(129, 5, 0, 3),
+(130, 6, 0, 3),
+(131, 1, 0, 4),
+(132, 2, 0, 4),
+(133, 3, 0, 4),
+(134, 4, 0, 4),
+(135, 1, 0, 5),
+(136, 2, 0, 5),
+(137, 3, 0, 5),
+(138, 4, 0, 5),
+(139, 5, 0, 5),
+(140, 6, 0, 5),
+(141, 1, 0, 6),
+(142, 2, 0, 6),
+(143, 3, 0, 6),
+(144, 4, 0, 6),
+(145, 5, 0, 6),
+(146, 6, 0, 6),
+(147, 7, 0, 6),
+(148, 1, 0, 7),
+(149, 2, 0, 7),
+(150, 3, 0, 7),
+(151, 4, 0, 7),
+(152, 1, 0, 8),
+(153, 2, 0, 8),
+(154, 3, 0, 8),
+(155, 4, 0, 8),
+(156, 5, 0, 8),
+(157, 6, 0, 8),
+(158, 7, 0, 8),
+(159, 8, 0, 8),
+(160, 1, 0, 9),
+(161, 2, 0, 9),
+(162, 3, 0, 9),
+(163, 4, 0, 9),
+(164, 1, 0, 10),
+(165, 2, 0, 10),
+(166, 3, 0, 10),
+(167, 4, 0, 10),
+(168, 5, 0, 10),
+(169, 6, 0, 10),
+(170, 1, 0, 11),
+(171, 2, 0, 11),
+(172, 3, 0, 11),
+(173, 4, 0, 11),
+(174, 5, 0, 11),
+(175, 1, 0, 12),
+(176, 2, 0, 12),
+(177, 3, 0, 12),
+(178, 4, 0, 12),
+(179, 1, 0, 13),
+(180, 2, 0, 13),
+(181, 3, 0, 13),
+(182, 4, 0, 13),
+(183, 5, 0, 13),
+(184, 1, 0, 14),
+(185, 2, 0, 14),
+(186, 3, 0, 14),
+(187, 4, 0, 14),
+(188, 5, 0, 14),
+(189, 6, 0, 14),
+(190, 7, 0, 14),
+(191, 1, 0, 15),
+(192, 2, 0, 15),
+(193, 3, 0, 15),
+(194, 4, 0, 15),
+(195, 5, 0, 15),
+(196, 1, 0, 16),
+(197, 2, 0, 16),
+(198, 3, 0, 16),
+(199, 4, 0, 16),
+(200, 1, 0, 17),
+(201, 2, 0, 17),
+(202, 3, 0, 17),
+(203, 4, 0, 17),
+(204, 5, 0, 17),
+(205, 1, 0, 18),
+(206, 2, 0, 18),
+(207, 3, 0, 18),
+(208, 4, 0, 18),
+(209, 5, 0, 18),
+(210, 6, 0, 18),
+(211, 7, 0, 18),
+(212, 1, 0, 19),
+(213, 2, 0, 19),
+(214, 3, 0, 19),
+(215, 4, 0, 19),
+(216, 5, 0, 19),
+(217, 1, 0, 20),
+(218, 2, 0, 20),
+(219, 3, 0, 20),
+(220, 4, 0, 20),
+(221, 5, 0, 20),
+(222, 6, 0, 20),
+(223, 7, 0, 20),
+(224, 1, 0, 21),
+(225, 2, 0, 21),
+(226, 3, 0, 21),
+(227, 4, 0, 21),
+(228, 5, 0, 21),
+(229, 1, 0, 22),
+(230, 2, 0, 22),
+(231, 3, 0, 22),
+(232, 4, 0, 22),
+(233, 1, 0, 23),
+(234, 2, 0, 23),
+(235, 3, 0, 23),
+(236, 4, 0, 23),
+(237, 5, 0, 23),
+(238, 6, 0, 23),
+(239, 7, 0, 23),
+(240, 1, 0, 24),
+(241, 2, 0, 24),
+(242, 3, 0, 24),
+(243, 1, 0, 25),
+(244, 2, 0, 25),
+(245, 3, 0, 25),
+(246, 4, 0, 25),
+(247, 5, 0, 25),
+(248, 6, 0, 25);
 
 -- --------------------------------------------------------
 
@@ -70,6 +222,37 @@ CREATE TABLE `STREET` (
   `zone_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Zrzut danych tabeli `STREET`
+--
+
+INSERT INTO `STREET` (`id`, `name`, `zone_id`) VALUES
+(1, 'Ulica Augustiańska', 1),
+(2, 'Ulica Bartosza', 1),
+(3, 'Ulica Bonifraterska', 1),
+(4, 'Ulica Adama Chmielowskiego', 1),
+(5, 'Ulica Dajwór', 2),
+(6, 'Ulica Estery', 2),
+(7, 'Plac Nowy', 2),
+(8, 'Ulica Starowiślna', 2),
+(9, 'Ulica Długa', 3),
+(10, 'Ulica Krowoderska', 3),
+(11, 'Ulica Słowiańska', 3),
+(12, 'Ulica Krótka', 4),
+(13, 'Ulica Krzywa', 4),
+(14, 'Ulica św. Filipa', 4),
+(15, 'Ulica Szlak', 5),
+(16, 'Ulica Pędzichów', 5),
+(17, 'Ulica Stefana Staszica', 5),
+(18, 'Ulica Łobzowska', 5),
+(19, 'Ulica Sienna', 8),
+(20, 'Mały Rynek', 8),
+(21, 'Ulica Mikołajska', 8),
+(22, 'Ulica Szpitalna', 8),
+(23, 'Ulica św. Tomasza', 9),
+(24, 'Ulica św. Krzyża', 9),
+(25, 'Plac św. Ducha', 9);
+
 -- --------------------------------------------------------
 
 --
@@ -80,8 +263,19 @@ CREATE TABLE `TICKET_TYPE` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `price` double NOT NULL,
-  `duration_hours` int(3) NOT NULL
+  `duration_hours` int(3) NOT NULL,
+  `duration` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `TICKET_TYPE`
+--
+
+INSERT INTO `TICKET_TYPE` (`id`, `name`, `price`, `duration_hours`, `duration`) VALUES
+(1, '1 hour parking time', 1.5, 1, 0),
+(2, '2 hour parking time', 3, 2, 0),
+(3, '3 hour parking time', 6, 3, 0),
+(4, '1 day parking time', 60, 24, 0);
 
 -- --------------------------------------------------------
 
@@ -94,6 +288,19 @@ CREATE TABLE `ZONE` (
   `name` varchar(50) NOT NULL,
   `employee_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `ZONE`
+--
+
+INSERT INTO `ZONE` (`id`, `name`, `employee_id`) VALUES
+(1, 'Kazimierz strefa 1', 1),
+(2, 'Kazimierz strefa 2', 1),
+(3, 'Kleparz strefa 1', 2),
+(4, 'Kleparz strefa 2', 2),
+(5, 'Kleparz strefa 3', 2),
+(8, 'Stare Miasto strefa 1', 3),
+(9, 'Stare Miasto strefa 2', 3);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -131,7 +338,9 @@ ALTER TABLE `STREET`
 -- Indexes for table `TICKET_TYPE`
 --
 ALTER TABLE `TICKET_TYPE`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD UNIQUE KEY `duration_hours` (`duration_hours`);
 
 --
 -- Indexes for table `ZONE`
@@ -148,12 +357,12 @@ ALTER TABLE `ZONE`
 -- AUTO_INCREMENT dla tabeli `EMPLOYEE`
 --
 ALTER TABLE `EMPLOYEE`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT dla tabeli `PARKING_SPOT`
 --
 ALTER TABLE `PARKING_SPOT`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=249;
 --
 -- AUTO_INCREMENT dla tabeli `PARKING_TICKET`
 --
@@ -163,17 +372,46 @@ ALTER TABLE `PARKING_TICKET`
 -- AUTO_INCREMENT dla tabeli `STREET`
 --
 ALTER TABLE `STREET`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 --
 -- AUTO_INCREMENT dla tabeli `TICKET_TYPE`
 --
 ALTER TABLE `TICKET_TYPE`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT dla tabeli `ZONE`
 --
 ALTER TABLE `ZONE`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `PARKING_SPOT`
+--
+ALTER TABLE `PARKING_SPOT`
+  ADD CONSTRAINT `FK25d5vkebmro1qtvuisbhalo6c` FOREIGN KEY (`street_id`) REFERENCES `STREET` (`id`);
+
+--
+-- Ograniczenia dla tabeli `PARKING_TICKET`
+--
+ALTER TABLE `PARKING_TICKET`
+  ADD CONSTRAINT `FK6jf8x6tvmi6cfvtk5pc3nairl` FOREIGN KEY (`ticket_type_id`) REFERENCES `TICKET_TYPE` (`id`),
+  ADD CONSTRAINT `FKhy73v0dkg87d4fqkh4htm41hd` FOREIGN KEY (`parking_spot_id`) REFERENCES `PARKING_SPOT` (`id`);
+
+--
+-- Ograniczenia dla tabeli `STREET`
+--
+ALTER TABLE `STREET`
+  ADD CONSTRAINT `FK33h8kypmu937k28m5bqiu6af2` FOREIGN KEY (`zone_id`) REFERENCES `ZONE` (`id`);
+
+--
+-- Ograniczenia dla tabeli `ZONE`
+--
+ALTER TABLE `ZONE`
+  ADD CONSTRAINT `FK7finhvmw3erxui3yfm24pirh5` FOREIGN KEY (`employee_id`) REFERENCES `EMPLOYEE` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
