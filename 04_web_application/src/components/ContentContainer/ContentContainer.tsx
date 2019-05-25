80ba0cb34ad6905e19c818c27f59c7044afaec6b
SkalskiP <piotr.skalski92@gmail.com>
2019-05-25 14:28:50 +0200

    Rounded box

--- 04_web_application/src/components/ContentContainer/ContentContainer.tsx
+++ 04_web_application/src/components/ContentContainer/ContentContainer.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './ContentContainer.scss';
 import {ContentContainerHeader} from "../ContentContainerHeader/ContentContainerHeader";
+import RoundBox from "../RoundBox/RoundBox";
 
 const ContentContainer: React.FC = () => {
     return (
         <div className="ContentContainer">
             <ContentContainerHeader/>
+            <RoundBox/>
         </div>
     );
 };
---
+++ 04_web_application/src/components/RoundBox/RoundBox.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './RoundBox.scss';
+
+const RoundBox: React.FC = () => {
+    return (
+        <div className="RoundBox"/>
+    );
+};
+
+export default RoundBox;
