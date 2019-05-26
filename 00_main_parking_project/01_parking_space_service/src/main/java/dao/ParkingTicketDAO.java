cd1d899a7006f1af7f5573c4a1ec49a96a21dc3a
SkalskiP <piotr.skalski92@gmail.com>
2019-05-26 22:21:46 +0200

    Working ticket filtration by date with pagination

--- 00_main_parking_project/01_parking_space_service/src/main/java/dao/ParkingTicketDAO.java
+++ 00_main_parking_project/01_parking_space_service/src/main/java/dao/ParkingTicketDAO.java
@@ -4,9 +4,14 @@ import domain.ParkingTicketStatus;
 import dto.ParkingSpotDTO;
 import dto.ParkingTicketDTO;
 
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class ParkingTicketDAO extends AbstractDAO<ParkingTicketDTO> {
 
@@ -35,5 +40,15 @@ public class ParkingTicketDAO extends AbstractDAO<ParkingTicketDTO> {
         query.select(b).where(criteriaBuilder.notEqual(b.get("status"), ParkingTicketStatus.CLOSED));
 
         return entityManager.createQuery(query).getSingleResult();
+    }
+
+    public List<ParkingTicketDTO> findTicketsFromTimePeriod(LocalDateTime from, LocalDateTime to, Integer limit, Integer offset) {
+        TypedQuery query = entityManager.createQuery(
+                "SELECT c FROM " + className + " c WHERE c.startTime BETWEEN :fromDate AND :toDate ORDER BY c.startTime DESC", clazz);
+        query.setParameter("fromDate", from);
+        query.setParameter("toDate", to);
+        query.setFirstResult(offset); // equivalent to OFFSET
+        query.setMaxResults(limit); // equivalent to LIMIT
+        return query.getResultList();
     }
 }
--- 00_main_parking_project/01_parking_space_service/src/main/java/rest/parkingTicket/ParkingTicketRestService.java
+++ 00_main_parking_project/01_parking_space_service/src/main/java/rest/parkingTicket/ParkingTicketRestService.java
@@ -3,12 +3,11 @@ package rest.parkingTicket;
 import dao.ParkingTicketDAO;
 import dto.ParkingTicketDTO;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Path("/parking-tickets")
@@ -27,5 +26,18 @@ public class ParkingTicketRestService {
     public Response getParkingTicketById(@PathParam("id") Integer id) {
         ParkingTicketDTO parkingTicket = ParkingTicketDAO.getInstance().getItem(id);
         return Response.ok().entity(parkingTicket).build();
+    }
+
+    @GET
+    @Path("/report")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getParkingTicketsReport(@QueryParam("from") String from, @QueryParam("to") String to,
+                                            @QueryParam("limit") Integer limit,  @QueryParam("offset") Integer offset) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime fromDateTime = LocalDateTime.parse(from, formatter);
+        LocalDateTime toDateTime = LocalDateTime.parse(to, formatter);
+        List<ParkingTicketDTO> parkingTickets = ParkingTicketDAO.getInstance()
+                .findTicketsFromTimePeriod(fromDateTime, toDateTime, limit, offset);
+        return Response.ok().entity(parkingTickets).build();
     }
 }
