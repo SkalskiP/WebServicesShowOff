4c6f0f2a250618146991642279153ab7e3db3f4d
SkalskiP <piotr.skalski92@gmail.com>
2019-06-02 00:52:46 +0200

    Base of notifications rest service added

---
+++ 00_main_parking_project/03_notifications_service/src/main/java/rest/ApiApplication.java
@@ -0,0 +1,36 @@
+package rest;
+
+import rest.employee.EmployeesRestService;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/rest")
+public class ApiApplication extends Application {
+
+    HashSet<Object> singletons = new HashSet<Object>();
+
+    public ApiApplication() {
+        CorsFilter corsFilter = new CorsFilter();
+        corsFilter.getAllowedOrigins().add("*");
+        corsFilter.setAllowedMethods("OPTIONS, GET, POST, DELETE, PUT, PATCH");
+        singletons.add(corsFilter);
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet<Class<?>> set = new HashSet<Class<?>>();
+
+        set.add(EmployeesRestService.class);
+
+        return set;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+
+}
