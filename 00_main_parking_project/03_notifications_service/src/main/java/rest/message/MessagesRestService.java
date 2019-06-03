16d3a6a185dcc1578c484631973345144c5842a1
SkalskiP <piotr.skalski92@gmail.com>
2019-06-03 21:36:40 +0200

    Notifications cross validation with parking space service added

--- 00_main_parking_project/03_notifications_service/src/main/java/rest/message/MessagesRestService.java
+++ 00_main_parking_project/03_notifications_service/src/main/java/rest/message/MessagesRestService.java
@@ -1,5 +1,8 @@
 package rest.message;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dto.ParkingSpotDTO;
+import jms.ParkingSystemNotificationMessage;
 import store.NotificationStore;
 
 import javax.ws.rs.GET;
@@ -8,6 +11,15 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 
 @Path("/employees")
@@ -16,13 +28,62 @@ public class MessagesRestService {
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public Response getMessagesAll() {
-        return Response.ok().entity(NotificationStore.getInstance().getNotificationsAll()).build();
+        List<ParkingSystemNotificationMessage> allNotifications = NotificationStore.getInstance().getNotificationsAll();
+        return Response.ok().entity(this.filterInactiveNotifications(allNotifications)).build();
     }
 
     @GET
     @Path("/{id}")
     @Produces({MediaType.APPLICATION_JSON})
     public Response getMessagesEmployeeById(@PathParam("id") Integer id) {
-        return Response.ok().entity(NotificationStore.getInstance().getNotificationsByEmployee(id)).build();
+        List<ParkingSystemNotificationMessage> allNotifications = NotificationStore.getInstance().getNotificationsByEmployee(id);
+        return Response.ok().entity(this.filterInactiveNotifications(allNotifications)).build();
     }
+
+    private List<ParkingSystemNotificationMessage> filterInactiveNotifications(List<ParkingSystemNotificationMessage> notifications) {
+        ArrayList activeNotifications = new ArrayList();
+
+        for (ParkingSystemNotificationMessage notificationMessage : notifications){
+            try {
+                Optional<ParkingSpotDTO> parkingSpot = this.sendGET(notificationMessage.getSpotId());
+                if (parkingSpot.isPresent() && parkingSpot.get().getOccupied() && Objects.equals(parkingSpot.get().getTriggerEventUuid(), notificationMessage.getTriggerEventUuid())) {
+                    activeNotifications.add(notificationMessage);
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return  activeNotifications;
+    }
+
+    private Optional<ParkingSpotDTO> sendGET(Integer spotId) throws IOException {
+        URL obj = new URL("http://localhost:8080/rest/parking-spots/" + spotId.toString());
+        ObjectMapper objectMapper = new ObjectMapper();
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            //TODO: Remove logs
+            System.out.println(response.toString());
+            return Optional.of(objectMapper.readValue(response.toString(), ParkingSpotDTO.class));
+        } else {
+            System.out.println("GET request not worked");
+            return Optional.empty();
+        }
+
+    }
+
 }
