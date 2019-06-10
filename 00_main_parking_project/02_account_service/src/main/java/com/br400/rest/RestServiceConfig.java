ceb276c83500ca50abaa4c13110b00f462e18014
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2019-06-10 21:00:59 +0200

    Fixed CORS filters stuff and removed idiotic bug

--- 00_main_parking_project/02_account_service/src/main/java/com/br400/rest/RestServiceConfig.java
+++ 00_main_parking_project/02_account_service/src/main/java/com/br400/rest/RestServiceConfig.java
@@ -1,35 +1,34 @@
 package com.br400.rest;
 
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 @ApplicationPath("/rest")
 public class RestServiceConfig extends Application {
-
-    HashSet<Object> singletons = new HashSet<Object>();
-
-    public RestServiceConfig() {
-        CorsFilter corsFilter = new CorsFilter();
-        corsFilter.getAllowedOrigins().add("*");
-        corsFilter.setAllowedMethods("OPTIONS, GET, POST, DELETE, PUT, PATCH");
-        singletons.add(corsFilter);
-    }
+    Set<Object> singletons;
 
     @Override
     public Set<Class<?>> getClasses() {
-        HashSet<Class<?>> set = new HashSet<Class<?>>();
-
-        set.add(UserService.class);
-        set.add(VerificationService.class);
-
-        return set;
+        HashSet<Class<?>> clazzes = new HashSet<>();
+        clazzes.add(UserService.class);
+        clazzes.add(VerificationService.class);
+        return clazzes;
     }
 
     @Override
     public Set<Object> getSingletons() {
+        if (singletons == null) {
+            CorsFilter corsFilter = new CorsFilter();
+            corsFilter.getAllowedOrigins().add("*");
+
+            singletons = new LinkedHashSet<>();
+            singletons.add(corsFilter);
+        }
         return singletons;
     }
-
 }
